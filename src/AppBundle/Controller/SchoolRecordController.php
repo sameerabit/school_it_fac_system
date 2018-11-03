d9a2ada1f8884785b3e6cd5044c6acf25408a282
Viraj Amarasinghe <virajamara@gmail.com>
2018-11-03 22:13:08 +0530

    unwanted import classes removed

--- src/AppBundle/Controller/SchoolRecordController.php
+++ src/AppBundle/Controller/SchoolRecordController.php
@@ -4,8 +4,7 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\SchoolRecord;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Schoolrecord controller.
